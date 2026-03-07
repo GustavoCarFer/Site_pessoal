17cf65c5548b23b7a5d1a7f8b4fca54af9cdb892
Gustavo Carvalho <guscafer@gmail.com>
2026-03-07 20:20:19 -0300

    Adicionado as funcionalidades com JS

---
+++ script.js
@@ -0,0 +1,16 @@
+const anoAtual = new Date().getFullYear();
+document.getElementById('year').textContent = anoAtual;
+
+const observer = new IntersectionObserver(function(entries) {
+    entries.forEach(function(entry){
+        if (entry.isIntersecting){
+            entry.target.classList.add('visivel');
+            observer.unobserve(entry.target);
+
+        }
+    });
+}, {threshold: 0.1});
+
+document.querySelectorAll('.fade-up').forEach(function(elemento){
+    observer.observe(elemento)
+})
