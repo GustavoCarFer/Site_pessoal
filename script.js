263ee8bdc2cdf9a5eecc7550cc16d9f70176d178
Gustavo Carvalho <guscafer@gmail.com>
2026-03-07 20:54:13 -0300

    Adicionando responsividade no projeto

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ const observer = new IntersectionObserver(function(entries) {
 document.querySelectorAll('.fade-up').forEach(function(elemento){
     observer.observe(elemento)
 })
+
+const hamburguer = document.getElementById('hamburguer');
+const navMenu = document.getElementById('nav-menu');
+
+hamburguer.addEventListener('click', function(){
+    navMenu.classList.toggle('aberto');
+})
+
+navMenu.querySelectorAll('a').forEach(function(link){
+    link.addEventListener('click', function(){
+        navMenu.classList.remove('aberto');
+    })
+})
